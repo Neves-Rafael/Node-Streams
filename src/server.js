0f443d5e19f6e5205af01f13b63162881e27d8f0
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-19 20:59:15 -0300

    :sparkles: Feat db in memory

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import http from "node:http"
 import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
 
-const users = [];
+const database = new Database(); 
 
 const server = http.createServer(async(req, res)=>{
   const { method, url } = req;
@@ -9,17 +11,22 @@ const server = http.createServer(async(req, res)=>{
   await json(req, res)
 
   if(method === "GET" && url ==="/users"){
+    const users = database.select("users");
+
     return res.end(JSON.stringify(users))
   }
 
   if(method === "POST" && url ==="/users"){
     const { name, email } = req.body;
 
-    users.push({
-      id: 1,
+    const user = ({
+      id: randomUUID(),
       name,
       email,
     })
+
+    database.insert("users", user)
+
     return res.writeHead(201).end();
   }
 
