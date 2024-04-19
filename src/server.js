04f9bd05717d1c8d4e79e6a9311e78a7ef39ba95
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-18 22:00:42 -0300

    :sparkles: Feat middleware

--- src/server.js
+++ src/server.js
@@ -1,21 +1,24 @@
 import http from "node:http"
+import { json } from "./middlewares/json.js";
 
 const users = [];
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer(async(req, res)=>{
   const { method, url } = req;
 
+  await json(req, res)
+
   if(method === "GET" && url ==="/users"){
-    return res
-      .setHeader("Content-Type", "application/json")
-      .end(JSON.stringify(users))
+    return res.end(JSON.stringify(users))
   }
 
   if(method === "POST" && url ==="/users"){
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name,
+      email,
     })
     return res.writeHead(201).end();
   }
